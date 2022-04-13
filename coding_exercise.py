5b8b80785491589f8e7ea750801c857676e2565f
aurorabaylessedwards <65464320+aurorabaylessedwards@users.noreply.github.com>
2022-04-12 19:46:25 -0700

    Update coding_exercise.py

--- coding_exercise.py
+++ coding_exercise.py
@@ -20,8 +20,8 @@ import os
 
 
 # list the url of the buildings and points csv files.
-# url_b = 'https://gist.githubusercontent.com/gberardinelli/8567cdbcad220e46b2f8fc4e33a203a0/raw/e9d27e14fbbde4b28a6c09d077b43537310a5b0b/buildings.csv'
-# url_q = 'https://gist.githubusercontent.com/gberardinelli/8567cdbcad220e46b2f8fc4e33a203a0/raw/e9d27e14fbbde4b28a6c09d077b43537310a5b0b/queries.csv'
+url_b = 'https://raw.githubusercontent.com/aurorabaylessedwards/Coding_Exercise/master/buildings_in_pdx.csv'
+url_q = 'https://raw.githubusercontent.com/aurorabaylessedwards/Coding_Exercise/master/randomPoints.csv'
 
 # read in the csv files as pandas data frames
 buildings = pd.read_csv(url_b, error_bad_lines=False)
@@ -32,7 +32,7 @@ points = pd.read_csv(url_q, error_bad_lines=False)
 def create_gdf(df, x="X", y="Y"):
     return gpd.GeoDataFrame(df,
                             geometry=gpd.points_from_xy(df[y], df[x]),
-                            crs={"init": "EPSG:2991"})
+                            crs={"init": "EPSG:6559"})
 
 # make your Geodataframes
 buildings_gdf = create_gdf(buildings)
